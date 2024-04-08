db8a06b901fa39e849b1f99dfa9b67e208d04ab1
priyansh1317 <160261926+priyansh1317@users.noreply.github.com>
2024-04-09 00:07:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (37.52%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.java
+++ 2556-convert-the-temperature/convert-the-temperature.java
@@ -1,8 +1,8 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double [] arr = new double[2];
-        arr[0] = celsius + 273.15;
-        arr[1] = celsius * 1.80 + 32;
-        return arr;
+        return new double[]{celsius + 273.15, celsius * 1.80 + 32 };
+        // arr[0] = celsius + 273.15;
+        // arr[1] = celsius * 1.80 + 32;
+        // return arr;
     }
 }
