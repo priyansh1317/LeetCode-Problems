6b4a2b4c809a678bca7c67414961e6ea71b0b1c4
priyansh1317 <160261926+priyansh1317@users.noreply.github.com>
2024-04-10 01:22:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (68.07%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        int min = Integer.MIN_VALUE/10;
+        int max = Integer.MAX_VALUE/10;
+        int rev = 0;
+        while(x!=0){
+            if(rev>max || rev < min){
+            return 0;
+            }
+            rev = rev*10 + x%10; 
+            x/=10;
+            
+        
+        }
+        
+        return rev;
+    }
+}
